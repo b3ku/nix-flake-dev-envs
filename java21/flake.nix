e16373d3a327871c64ee5e5d9fe75cefcfb89b7c
Andreas Kuhrwahl <andreas.kuhrwahl@gmail.com>
2024-10-01 09:05:06 +0200

    use flake-utils for java21

--- java21/flake.nix
+++ java21/flake.nix
@@ -1,14 +1,19 @@
 {
   description = "A flake with a jdk";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { nixpkgs, ... }: let
-    system = "aarch64-darwin";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShell.${system} = with pkgs; mkShell {
-      buildInputs = [ jdk21_headless gradle maven ];
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = with pkgs; mkShell {
+          packages = [ jdk21_headless gradle maven ];
+        };
+      }
+    );
 }
